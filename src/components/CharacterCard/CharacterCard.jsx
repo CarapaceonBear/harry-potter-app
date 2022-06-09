c5526c78eb921834132365171ebd522fddf104b0
carapaceonbear <seerhan17@gmail.com>
2022-06-09 14:59:07 +0100

    all character cards showing with info from data

--- src/components/CharacterCard/CharacterCard.jsx
+++ src/components/CharacterCard/CharacterCard.jsx
@@ -3,9 +3,15 @@ import "./CharacterCard.scss"
 
 const CharacterCard = ({ character }) => {
 
+  const {name, image, house, patronus, actor} = character;
+
   return (
-    <div>
-      
+    <div className="card">
+      <h1 className="card__name">{name}</h1>
+      <img className="card__portrait" src={image} alt={name} />
+      <h2 className="card__house">{house}</h2>
+      <h3 className="card__info">Patronus: {patronus}</h3>
+      <h3 className="card_info">Played by: {actor}</h3>
     </div>
   )
 }
