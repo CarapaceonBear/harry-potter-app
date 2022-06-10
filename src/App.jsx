0cc744f2df74155e5c8b7808ca5ee32f67433ceb
carapaceonbear <seerhan17@gmail.com>
2022-06-10 12:20:15 +0100

    filter by house working, filter moved into Characters component

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,34 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import "./App.scss";
 import Characters from "./containers/Characters/Characters";
+import logo from "./assets/images/HarryPotterLogo.png";
 
 function App() {
-  const [characterArray, setArray] = useState();
+  const [house, setHouse] = useState("none");
 
-  useEffect(() => {
-    getCharacters().then(items => setArray(items))
-  }, []);
-
-  const getCharacters = async () => {
-    const data = await fetch("https://hp-api.herokuapp.com/api/characters");
-    return await data.json();
-  };
+  const FilterByHouse = (event) => {
+    console.log(event.target.value);
+    setHouse(event.target.value);
+  }
 
 
   return(
     <div className="App">
       <section className="heading" >
+        <img className="heading__logo" src={logo} alt="Harry Potter" />
         <h1 className="heading__title" >Harry Potter</h1>
+        <select className="heading__house-menu" name="house" id="house-menu" onChange={FilterByHouse}>
+          <option value="none">filter by house</option>
+          <option value="gryffindor">Gryffindor</option>
+          <option value="ravenclaw">Ravenclaw</option>
+          <option value="slytherin">Slytherin</option>
+          <option value="hufflepuff">Hufflepuff</option>
+        </select>
         {/* search box */}
       </section>
 
       <section className="gallery" >
-        <Characters characters={characterArray} />
+        <Characters house={house} />
       </section>
 
     </div>
--- src/containers/Characters/Characters.jsx
+++ src/containers/Characters/Characters.jsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import "./Characters.scss"
 import CharacterCard from '../../components/CharacterCard/CharacterCard';
 
-const Characters = ({ characters }) => {
+const Characters = ({ house }) => {
+  const [characterArray, setArray] = useState();
+
+  useEffect(() => {
+    getCharacters().then(items => setArray(items))
+  }, [house]);
+
+  const getCharacters = async () => {
+    let data = null;
+    switch (house) {
+      case "none":
+        data = await fetch("https://hp-api.herokuapp.com/api/characters");
+        break;
+      case "gryffindor":
+        data = await fetch("https://hp-api.herokuapp.com/api/characters/house/gryffindor");
+        break;
+      case "ravenclaw":
+        data = await fetch("https://hp-api.herokuapp.com/api/characters/house/ravenclaw");
+        break;
+      case "slytherin":
+        data = await fetch("https://hp-api.herokuapp.com/api/characters/house/slytherin");
+        break;
+      case "hufflepuff":
+        data = await fetch("https://hp-api.herokuapp.com/api/characters/house/hufflepuff");
+        break;
+    }
+    console.log("test");
+    return await data.json();
+  };
   
   return (
     <div className="characters">
-      {(characters || []).map((character, index) => {
+      {(characterArray || []).map((character, index) => {
         return <CharacterCard key={index} character={character}/>
       })}
     </div>
